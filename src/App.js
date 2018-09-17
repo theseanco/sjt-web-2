3531ba8dfefa37872d04fe888ec1ee9aab450f8f
theseanco <theseancotterill@gmail.com>
2018-09-17 10:56:31 +0100

    tempo slider working, beginning work on volume slider

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ import Tone from 'tone'
 
 TODO:
 
-- Tempo slider
+x Tempo slider
+- CHANGING TEMPO SLIDER MECHANISM
 - Make things not look dreadful
   - Pre-align four divs and bottom tempo/volume div
   - Color scheme, nice buttons
@@ -35,13 +36,23 @@ const startPlaying = () => {
   console.log("started")
 }
 
+/*
+A default tempo value.
+
+TODO: Perhaps this isn't the best way to do this, starting with a hardcoded value, but it works for now.
+*/
+const defaultTempo = 100;
+const defaultVolume = 80;
+
 class App extends Component {
 
   constructor (props) {
     super(props);
     this.state = {
       //Booleans control whether loops are activated or not
-      arrayOfIndexes: [false , false, false, false]
+      arrayOfIndexes: [false , false, false, false],
+      tempoValue: defaultTempo,
+      volume: defaultVolume
     }
   }
 
@@ -73,13 +84,50 @@ class App extends Component {
     this.setState({arrayOfIndexes: stateArray})
   }
 
+
   testFunction = (index) => {
     console.log("works", index);
   }
 
+  //function to set the bpm of  the transport and apply it to state to be printed to the page.
+  //TODO: This needs to be a ramp if the values are too far apart. Perhaps a progressive ramp
+  /*
+
+  Working out:
+
+  diff / 10:
+
+  2 = 0.2s
+  160 = 16.0s
+
+  */
+  setBpm = (e) => {
+    const tempoValue = parseInt(e.target.value);
+    const currentState = this.state.tempoValue;
+    const difference = Math.abs(tempoValue - currentState);
+
+    //Check the difference in the values. If it's more than 2, it's a good idea to ramp it.
+    if(difference <= 2) {
+      //no ramp
+      Tone.Transport.bpm.value = tempoValue;
+      this.setState({tempoValue: tempoValue});
+    } else {
+      //ramp
+      this.setState({tempoValue: tempoValue});
+      Tone.Transport.bpm.rampTo(tempoValue,difference/50)
+    }
+
+    console.log(Math.abs(tempoValue - currentState));
+  }
+
+  componentDidMount() {
+    startPlaying();
+    //This needs
+    Tone.Transport.bpm.value = defaultTempo;
+  }
+
   render() {
 
-    startPlaying();
 
     return (
       <div>
@@ -101,7 +149,19 @@ class App extends Component {
             }
           })
         }
-              </div>
+        <div>
+          <div>
+          <p>Tempo Slider</p>
+          <input type="range" min="40" max="160" value={defaultTempo} onChange={this.setBpm}></input>
+          {this.state.tempoValue}
+        </div>
+        <div>
+          <p>Volume slider</p>
+          <input type="range" min="0" max="100" value={defaultVolume} onChange={this.setVolume}></input>
+          {this.state.volume}
+        </div>
+        </div>
+        </div>
         </div>
     );
   }
