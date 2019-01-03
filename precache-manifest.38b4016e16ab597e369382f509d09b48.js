ee784a3a7f4c9db30a1f60d9d5275717817aeb18
theseanco <theseancotterill@gmail.com>
2019-01-03 13:20:21 +0000

    Updates

---
+++ precache-manifest.38b4016e16ab597e369382f509d09b48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f38bc5072c8c63c4ef6",
+    "url": "/sjt-web-2/static/js/runtime~main.3f38bc50.js"
+  },
+  {
+    "revision": "3d6dd4f158dfa3af4d95",
+    "url": "/sjt-web-2/static/js/main.3d6dd4f1.chunk.js"
+  },
+  {
+    "revision": "5388b0130be5064d916d",
+    "url": "/sjt-web-2/static/js/1.5388b013.chunk.js"
+  },
+  {
+    "revision": "3d6dd4f158dfa3af4d95",
+    "url": "/sjt-web-2/static/css/main.30035461.chunk.css"
+  },
+  {
+    "revision": "ac4af49781e4a038b070b4ff52d3e075",
+    "url": "/sjt-web-2/index.html"
+  }
+];
